bca36e9c70961f7a1266bd517399e3f5b4a6d45b
Nick Levshin <nikita.levshin2016@gmail.com>
2022-05-15 20:57:04 +0300

    feat: create classes Вс 15 мая 2022 20:57:04 MSK

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import CellComponent from './CellComponent';
+import { Board } from '../models/Board';
 
-const BoardComponent = () => {
+interface BoardProps {
+  board: Board;
+  setBoard: (board: Board) => void;
+}
+
+const BoardComponent: React.FC<BoardProps> = ({ board, setBoard }) => {
   return (
     <div className="board">
-      <div className="cell white"></div>
-      <div className="cell black"></div>
-      <div className="cell white"></div>
-      <div className="cell black"></div>
+      {board.cells.map((row, index) => (
+        <React.Fragment key={index}>
+          {row.map((cell) => (
+            <CellComponent cell={cell} key={cell.id} />
+          ))}
+        </React.Fragment>
+      ))}
     </div>
   );
 };
